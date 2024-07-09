fe80a234f3eea9cb07ca612d681b0c509475eead
KAVIRAJec <kaviraj.ec21@bitsathy.ac.in>
2024-07-09 11:26:58 +0530

    added about section[ADD]

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,11 +42,11 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
+      title: "Fullstack Developer",
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Frontend Developer",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Cloud & DevOps Developer",
       icon: creator,
     },
   ];
